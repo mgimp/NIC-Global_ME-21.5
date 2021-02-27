50c2dfb5466b7b5e6adc329770672fee803f44de
mgimp <70447460+mgimp@users.noreply.github.com>
2021-02-26 17:04:33 -0800

    Revert "General Setup for Homing Cycle"
    This reverts commit 753921d335de37c6601ccb06be4afa96cfdce5e5.

--- HomingCycle.c
+++ HomingCycle.c
@@ -1,24 +1,11 @@
-/*
-// SETUP STEPPER MOTOR VARIABLES
-SpeedyStepper gantryX;  // The big one
-SpeedyStepper gantryY;  // The small one
-SpeedyStepper pusher;
-SpeedyStepper tray;
-
-// SETUP STEPPER MOTOR SPEEDS AND ACCELERATIONS
-// CONVERSION: 17.5 steps/mm
-// Vmax = 8000 steps/s
-// Amax = 9500 steps/s^2
-gantryX.setSpeedInMillimetersPerSecond(8000/17.5);
-gantryX.setAccelerationInMillimetersPerSecondPerSecond(9500/17.5);
-
-// SETUP HOMING SWITCH VARIABLES
-
-*/
-
 switch (currState){
         case HOMING_CYCLE:
-            
+            // flash the LED until
+            digitalWrite(DO_RUNNING,!digitalRead(DO_RUNNING);
+            msDelay(500);
+            // I just add this for fun
 
-
+            // right now there is no way of exiting an error condition
+            // this is something to be decided
+            // for examlple could have the emergency stop button be latching and you need to undo it and then press start?
         break;
