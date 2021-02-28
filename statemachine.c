85bf9d82aac5ea66144448d52a19975067a0a520
mgimp <70447460+mgimp@users.noreply.github.com>
2021-02-27 18:54:03 -0800

    File creation
    Created the file initially with some starting comments that describe the script.

---
+++ statemachine.c
@@ -0,0 +1,21 @@
+/*
+BRIEF EXPLANATION FOR CLASSMATES
+I made this file so that we could construct the statemachine from the ground up rather than editing Greg's files.
+This ought to give us a greater understanding of the structure of the program, even if we end up copy/pasting
+a significant amount of the code.
+
+Seattle University
+    Mechanical Engineering Department
+    Engineering Design Group 21.5
+NIC Global
+    Vision System for Silkscreen Inspection
+David Schulman, Linh Ngo, Matthew Gimpelevich, Robert Wheeler
+27FEB2021
+
+THIS CODE is meant to service the Vision System for Silkscreen Inspection as designed by group 21.5
+It uses a basic state machine to control a camera, gantry systems, limit switch, and operating consol using an Arduino Mega
+Camera: 
+Actuator Model: FUYU FSL Series
+Stepper Motor: Nema 23
+Brain: Arduino Mega
+*/
