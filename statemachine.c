816ad6c04740d35c5896560912c1ce56a7b4a2d4
mgimp <70447460+mgimp@users.noreply.github.com>
2021-03-02 14:25:36 -0800

    Some setup and notes

--- statemachine.c
+++ statemachine.c
@@ -12,9 +12,18 @@ Seattle University
 27FEB2021
 
 THIS CODE is meant to service the Vision System for Silkscreen Inspection as designed by group 21.5
-It uses a basic state machine to control a camera, gantry systems, limit switch, and operating consol using an Arduino Mega and the SpeedyStepper library.
+It uses a basic state machine to control a camera, gantry systems, limit switch, and operating consol using an Arduino Mega and the SpeedyStepper library Copyright (c) 2014 Stanley Reifel & Co.
 Camera: 
 Actuator Model: FUYU FSL Series
 Stepper Motor: Nema 23
 Brain: Arduino Mega
 */
+
+// -----EQUIPMENT NOTES-----
+// Limit switches probably fail LOW
+
+// -----MOVEMENT-----
+// LOW = AWAY FROM MOTOR
+// HIGH = TOWARD MOTOR
+// NEGATIVE VALUES = TOWARD MOTOR
+// POSITIVE VALUES = AWAY FROM MOTOR
