34cb030361caa08e95d22754ac2e9bb14eef1775
Linh Ngo <linhngo_11@yahoo.com>
2021-03-04 20:56:43 -0800

    Implementation of Emergency Stop Button

---
+++ EmergencyStop.c
@@ -0,0 +1,40 @@
+// REFERENCE OF VARIABLES FROM statemachine.c
+// -----SPEEDYSTEPPER VARIABLES-----
+// Uses a typedef provided by SpeedyStepper
+SpeedyStepper ss_gantryy;  // 900mm
+SpeedyStepper ss_gantryx;  // 350mm
+SpeedyStepper ss_wiper; // 500mm
+SpeedyStepper ss_tray;  // 1000mm
+
+void EmergencyStop(void)
+{
+    // turn on RED LED indicating users have pressed emergency stop button
+    LED_System_Failure(1);
+    
+    // setupStop() will move the target position 
+    // so that the motor will begin deceleration now
+    ss_gantryy.setupStop();
+    ss_gantryx.setupStop();
+    ss_wiper.setupStop();
+    ss_tray.setupStop();
+
+    // processMovement() returns true if movement complete, 
+    // and false not a final target 
+    // position yet
+    if (ss_gantryy.processMovement() == true)
+    {
+        // motor is completely stop
+    }
+    if (ss_gantryx.processMovement() == true)
+    {
+        // motor is completely stop
+    }
+    if (ss_wiper.processMovement() == true)
+    {
+        // motor is completely stop
+    }
+    if (ss_tray.processMovement() == true)
+    {
+        // motor is completely stop
+    }
+}
