185217af5256fee6c81ed21fe18ee653e46eb527
Linh <linhngo_11@yahoo.com>
2021-04-06 10:52:24 -0700

    E.Stop fixed

--- oldfiles/EmergencyStop.c
+++ oldfiles/EmergencyStop.c
@@ -14,6 +14,17 @@
 
 case EMERGENCY_STOP_START:
     LED_System_Failure(1); // turn on the RED LED when E.stop button is pressed
+        // Assign IO pins used for Step and Direction
+        // Stop command from Arduino to motors by setting all pinmode to output
+    pinMode(DO_XGANTRY_DIR,OUTPUT);
+    pinMode(DO_XGANTRY_PUL,OUTPUT);
+    pinMode(DO_YGANTRY_DIR,OUTPUT);
+    pinMode(DO_yGANTRY_PUL,OUTPUT);
+    pinMode(DO_WIPER_DIR,OUTPUT);
+    pinMode(DO_WIPER_PUL,OUTPUT);
+    pinMode(DO_TRAY_DIR,OUTPUT);
+    pinMode(DO_TRAY_PULL,OUTPUT);
+
     //update to new case and then break
     currState = EMERGENCY_STOP_INPROGRESS; //go to emergency stop progress state
     break; 
@@ -33,7 +44,17 @@ case EMERGENCY_STOP_INPROGRESS:
     }
     break;
 
-case EMERGENCY_STOP_EXIT:
+case EMERGENCY_STOP_EXIT
+    pinMode(DO_XGANTRY_DIR,INPUT);
+    pinMode(DO_XGANTRY_PUL,INPUT);
+    pinMode(DO_YGANTRY_DIR,INPUT);
+    pinMode(DO_yGANTRY_PUL,INPUT);
+    pinMode(DO_WIPER_DIR,INPUT);
+    pinMode(DO_WIPER_PUL,INPUT);
+    pinMode(DO_TRAY_DIR,INPUT);
+    pinMode(DO_TRAY_PULL,INPUT);
+    initializeMotorSpeeds();
+    currState = START_HOMING_CYCLE;
     //How to release the emergency stop button?
     //Should it go to Homing Cycle after the emergency stop exit?
     //After motors are fixed, need some signal to turn off the emergency stop LED
